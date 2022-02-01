b19097daa9a3d096323a9ecd2e4f78fba9210e6d
Jeffrey Tilson <jtilson@renci.org>
2022-02-01 08:49:39 -0500

    Preliminary code for doing daily observation harvesting

---
+++ daily_observation_update.sh
@@ -0,0 +1,29 @@
+#
+# Setup the basic env values relative to /projects/sequence_analysis/vol1/prediction_work/HARVESTOR/fetch_station_data
+
+export PYTHONPATH=/projects/sequence_analysis/vol1/prediction_work/HARVESTOR/fetch_station_data
+#export RUNTIMEDIR=./DAILIES-TEST
+export RUNTIMEDIR=/projects/ees/TDS/DataHarvesting/DAILY_HARVESTING
+
+# Prepare invocation
+cd /projects/sequence_analysis/vol1/prediction_work/HARVESTOR/fetch_station_data
+
+# Dig out the DAY, MONTH, and YEAR values. Always set hour:min:sec to 00:00:00
+
+day="$(date +'%d')"
+month="$(date +'%m')"
+year="$(date +'%Y')"
+hour="$(date +'%H')"
+#printf "Current date in dd/mm/yyyy format %s\n" "$day-$month-$year $hour:00:00"
+printf "Current date in dd/mm/yyyy format %s\n" "$day-$month-$year 00:00:00"
+
+stoptime="$year-$month-$day 00:00:00"
+
+python fetch_data.py --data_source 'CONTRAILS' --data_product 'coastal_water_level' --stoptime "$stoptime"
+python fetch_data.py --data_source 'CONTRAILS' --data_product 'river_water_level' --stoptime "$stoptime"
+python fetch_data.py --data_source 'NOAA' --data_product 'water_level' --stoptime "$stoptime"
+
+echo "Finished"
+
+
+
