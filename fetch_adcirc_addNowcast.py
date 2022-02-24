83bff501cf22c6920b1d9302b83eeac204656b03
Jeffrey Tilson <jtilson@renci.org>
2022-02-24 11:16:41 -0500

    Modified the arg list to support fort63_style

--- fetch_adcirc_addNowcast.py
+++ fetch_adcirc_addNowcast.py
@@ -11,12 +11,14 @@ parser.add_argument('--sources', action='store_true',
                     help='List currently supported data sources')
 parser.add_argument('--data_source', action='store', dest='data_source', default='ASGS', type=str,
                     help='choose supported data source: default = ASGS')
-parser.add_argument('--url', action='store', dest='url', default=None, type=str,
+parser.add_argument('--urls', action='store', dest='urls', default=None, type=str,
                     help='ASGS url to fetcb ADCIRC data')
 parser.add_argument('--data_product', action='store', dest='data_product', default='water_level', type=str,
                     help='choose supported data product: default is water_level')
 parser.add_argument('--convertToNowcast', action='store_true',
                     help='Attempts to force input URL into a nowcast url assuming normal ASGS conventions')
+parser.add_argument('--fort63_style', action='store_true',
+                    help='Boolean: Will inform Harvester to use fort.63.methods to get station nodesids')
 #print(sys.argv[1:])
 args = parser.parse_args()
 #argList=sys.argv[1:]
